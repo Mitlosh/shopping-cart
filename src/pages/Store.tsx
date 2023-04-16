463b9c1c74af03e38cdfac9ac8fe80f7375afa80
Vladyslav <vlad.mitlosh@gmail.com>
2023-04-16 11:27:10 +0300

    Initial commit

---
+++ src/pages/Store.tsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react"
+import { Row, Col } from "react-bootstrap"
+import { StoreItem } from "../components/StoreItem"
+
+export function Store() {
+  const [products, setProducts] = useState<any[]>([])
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products/category/electronics")
+      .then((res) => res.json())
+      .then((data) => setProducts((prev) => data))
+  }, [])
+
+  return (
+    <>
+      <h1>Store</h1>
+      <Row md={2} sx={1} lg={3} className="g-3">
+        {products.map((product) => (
+          <Col key={product.id}>
+            <StoreItem {...product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  )
+}
